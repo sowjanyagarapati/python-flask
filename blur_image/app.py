04b71f6c79573caab0d13f9588205c196bc15c52
sowjanyagarapati <46521587+sowjanyagarapati@users.noreply.github.com>
2022-04-21 18:25:01 +0530

    Delete blur_image directory

--- blur_image/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "u dont know"
-UPLOAD_FOLDER = 'static\\uploads'
-BLUR_FOLDER = 'static\\blurred_images'
-app.config['UPLOAD_FOLDER'] = os.path.join(UPLOAD_FOLDER)
-app.config['BLUR_FOLDER'] = os.path.join(BLUR_FOLDER)
